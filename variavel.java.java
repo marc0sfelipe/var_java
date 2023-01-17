f53d5cdf0ec308597cb394e7afb60b4336c2130c
marc0sfelipe <marcos12004@hotmail.com>
2023-01-17 17:20:30 -0300

    var java

---
+++ variavel.java.java
@@ -0,0 +1,13 @@
+public class curso_java1 {
+    public static void main(String[] args) {
+        
+        int num=0;
+        String nome="Marcos";
+
+        System.out.println("Curso de Java");
+        System.out.print("Curso FB\n");
+        System.out.printf("%s - %d%n","CFB cursos", 2023);
+        System.out.printf("Valor da variavel num: %d%nNome:%s", num, nome);
+    }
+    
+}
